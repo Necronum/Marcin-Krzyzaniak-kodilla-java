aef61fd3995ea9c5f50c2f54295f0136665b1b2f
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2020-11-18 21:06:21 +0100

    7.2. Added immutable and iterate

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,4 +1,5 @@
 package com.kodilla.stream;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.reference.FunctionalCalculator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.beautifier.PoemBeautifier;
@@ -13,7 +14,7 @@ public class StreamMain {
         expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
 
-        System.out.println("Calculating expressions with method references");
+        System.out.println("\n Calculating expressions with method references");
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
@@ -26,5 +27,9 @@ public class StreamMain {
         poemBeautifier.beautify("PUPPY", String::toLowerCase);
         poemBeautifier.beautify("        this is text with spaces before", String::trim);
         poemBeautifier.beautify("dog", (text -> "This is a " + text + " ABC"));
+        poemBeautifier.beautify("dog", (text -> text.substring(0,1).toUpperCase() + text.substring(1)));
+
+        System.out.println("\n Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
