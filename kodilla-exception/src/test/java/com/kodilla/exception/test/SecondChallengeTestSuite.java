b7d6bda52a780af3b50cef645e1a3cbd6dc38141
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2020-11-26 14:19:46 +0100

    8.3 Added tests and new classes

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/SecondChallengeTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.exception.test;
+
+import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SecondChallengeTestSuite {
+    @Test
+    void testProbablyIWillThrowException(){
+        // given
+        SecondChallenge secondChallenge = new SecondChallenge();
+        // when & then
+        assertAll(
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2, 0)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(3, 0)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(0, 0)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1, 1.5)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(0, 1.5)),
+                () -> assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1, 0)));
+    };
+}
