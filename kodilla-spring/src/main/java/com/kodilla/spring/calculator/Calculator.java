e2377c7543c6b954c20da02cc8e17bb309fdef90
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-01-15 16:57:15 +0100

    14.2. Added library and calculator

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -0,0 +1,34 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    @Autowired
+    private Display display;
+
+    double add (double a, double b) {
+        double val = a+b;
+        display.displayValue(val);
+        return val;
+    }
+
+    double sub (double a, double b) {
+        double val = a-b;
+        display.displayValue(val);
+        return val;
+    }
+
+    double mul (double a, double b) {
+        double val = a*b;
+        display.displayValue(val);
+        return val;
+    }
+
+    double div (double a, double b) {
+        double val = a/b;
+        display.displayValue(val);
+        return val;
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Autowired
+    private Calculator calculator;
+
+    @Test
+    public void testCalculations(){
+        //Given
+        double a = 8;
+        double b = 2;
+        //When
+        calculator.add(a, b);
+        calculator.sub(a, b);
+        calculator.mul(a, b);
+        calculator.div(a, b);
+        //Then
+    }
+
+}
